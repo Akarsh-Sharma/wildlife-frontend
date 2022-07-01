a232ff4297eeadabe05b6490da38003cf18f24b1
Akarsh Sharma <akarsh.sharma@mail.utoronto.ca>
2022-06-30 20:10:28 -0400

    Added the upload image feature from firebase to personal gallery section.

--- src/App.js
+++ src/App.js
@@ -6,47 +6,11 @@ import PersonalGallery from "./containers/personalGallery/PersonalGallery";
 import PublicGallery from "./containers/publicGallery/PublicGallery";
 import Profile from "./containers/profile/Profile";
 import Navbar from './components/navbar/Navbar';
-import { useEffect, useState } from 'react';
-import { storage } from './firebaseConfig';
-import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage';
-import { v4 } from 'uuid';
 
 function App() {
 
-  const [imageUpload, setImageUpload] = useState(null);
-  const [imageList, setImageList] = useState([]);
-  const imageListRef = ref(storage, "images/");
-
-  const uploadImage = () => {
-    if(imageUpload==null) return;
-    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
-    uploadBytes(imageRef, imageUpload).then((snapshot) => {
-      getDownloadURL(snapshot.ref).then((url) => {
-        setImageList((prev) => [...prev, url]);
-      })
-      
-    });
-  };
-
-  // uploading the imageList in the "images/" folder on our firebase storage to our react app
-  useEffect(() => {
-      listAll(imageListRef).then((response) => {
-        response.items.forEach((item) => {
-          getDownloadURL(item).then((url) => {
-            setImageList((prev) => [...prev, url]);
-          })
-        })
-      })
-  }, []);
-
   return (
-    <div>
-      <input type="file" onChange={(event) => {setImageUpload(event.target.files[0])}}/>
-      <button onClick={uploadImage}>Upload Image</button>
-
-      {imageList.map((url) => {
-        return <img src = {url} />
-      })}
+    
       <Router> 
       {  /* Whatever is written between outside the <Routes> tag will be shown on every page. */ }
 
@@ -59,7 +23,7 @@ function App() {
           <Route path='/profile' element={<Profile/>} />
         </Routes>
       </Router>
-    </div>
+    
   );
 }
 
--- src/containers/personalGallery/PersonalGallery.jsx
+++ src/containers/personalGallery/PersonalGallery.jsx
@@ -1,11 +1,58 @@
 import React from 'react'
-import UploadImage from '../../components/uploadImage/UploadImage'
+import { useEffect, useState } from 'react';
+import { storage } from './firebaseConfig';
+import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage';
+import { v4 } from 'uuid';
+import './personalGallery.css'
+import { ImageList, ImageListItem } from '@mui/material';
+
+
 
 function PersonalGallery() {
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imageList, setImageList] = useState([]);
+  const imageListRef = ref(storage, "images/");
+
+  const uploadImage = () => {
+    if(imageUpload==null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setImageList((prev) => [...prev, url]);
+      });
+      
+    });
+  };
+
+  // uploading the imageList in the "images/" folder on our firebase storage to our react app
+  useEffect(() => {
+      listAll(imageListRef).then((response) => {
+        response.items.forEach((item) => {
+          getDownloadURL(item).then((url) => {
+            setImageList((prev) => [...prev, url]);
+          })
+        })
+      })
+  }, []);
   return (
     <div>
-      personalGallery
-      
+      <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+        {imageList.map((url) => (
+          <ImageListItem key={url}>
+            <img
+              src={`${url}?w=164&h=164&fit=crop&auto=format`}
+              srcSet={`${url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+      <input type="file" onChange={(event) => {
+        setImageUpload(event.target.files[0])
+        console.log(event.target.files[0].name);
+        }
+      }/>
+      <button onClick={uploadImage}>Upload Image</button>
     </div>
   )
 }
