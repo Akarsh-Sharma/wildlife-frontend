d92b475f1c476d14d3be7a23cad4fb89a373d8b1
Akarsh <akarsh311298@gmail.com>
2022-07-28 00:29:39 -0400

    removed unused import statements

--- backend/controllers/BackendSignIn.js
+++ backend/controllers/BackendSignIn.js
@@ -1,7 +1,4 @@
-import React, {Component} from 'react'
-import { useEffect, useState } from 'react';
-import { storage, db } from '../personalGallery/firebaseConfig';
-import { collection, getDocs, addDoc } from 'firebase/firestore';
+
 
 export const SignIn = (req, res) => {
     const {email, password} = req.body; 
